24ee6b628a4c867aa9962f99edf79685af8d871a
Ethan <word.of.ethan@gmail.com>
2024-04-04 21:41:10 +0800

    Update diy-script.sh

--- diy-script.sh
+++ diy-script.sh
@@ -59,7 +59,7 @@ function git_sparse_clone() {
 
 # 添加额外插件
 # Adguardhome
-git clone --depth=1 https://github.com/kenzok8/openwrt-packages package/luci-app-adguardhome
+git clone --depth=1 https://github.com/kongfl888/luci-app-adguardhome package/luci-app-adguardhome
 # git clone --depth=1 https://github.com/kenzok8/openwrt-packages package/adguardhome
 
 # Alist
