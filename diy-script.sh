87c20d7eaaac5bbe783581a1b3991e889223d0a1
Ethan <word.of.ethan@gmail.com>
2024-04-05 14:39:24 +0800

    Update diy-script.sh

--- diy-script.sh
+++ diy-script.sh
@@ -18,6 +18,7 @@ sed -i 's/\/bin\/ash/\/usr\/bin\/zsh/g' package/base-files/files/etc/passwd
 
 # 移除要替换的包
 rm -rf feeds/packages/net/mosdns
+rm -rf feeds/packages/net/adguardhome
 rm -rf feeds/packages/utils/v2dat
 rm -rf feeds/packages/v2dat
 rm -rf feeds/luci/applications/luci-app-mosdns
@@ -57,7 +58,7 @@ function git_sparse_clone() {
 # 添加额外插件
 # Adguardhome
 git clone --depth=1 https://github.com/sirpdboy/sirpdboy-package package/luci-app-adguardhome
-# git clone --depth=1 https://github.com/kenzok8/openwrt-packages package/adguardhome
+git clone --depth=1 https://github.com/xuanranran/lede-package package/adguardhome
 
 # Alist
 git clone --depth=1 https://github.com/sbwml/luci-app-alist package/luci-app-alist
